392a83ba60836455a3016c22f06e61fa555345d9
Kuba Zabava <jakubphan@protonmail.com>
2022-04-08 13:24:52 +0200

    created list_all_interfaces function and finished argument parser in main

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "ipk-sniffer.hpp"
 #include <string.h>
+#define HAVE_REMOTE
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <array>
+#include <vector>
+#include <algorithm> //std::find()
+
 
 using namespace std;
 
@@ -17,30 +24,86 @@ int main(int argc, char* argv[]) {
             // checking next optional argument for -i/--interface
             else {
                 string opt_arg = argv[next_index];
-                cout << "-i ARG: " + opt_arg + "\n";
+                if (!isMember(opt_arg, args_list)) {
+                    arg.interface = opt_arg;
+                    i++;
+                }
             }
         }
         else if (argum.compare("-p")==0) {
-            cout << "port\n";
+            // if not given as argument
+            if ((i+1) != argc) {
+                if (!isMember(argv[i+1], args_list)) {
+                    arg.port = stoi(argv[i+1]);
+                    i++;
+                }
+            }
+        }
+        else if (argum.compare("-n")==0) {
+            // if not given as argument
+            if ((i+1)!=argc) {
+                if (!isMember(argv[i+1], args_list)) {
+                    arg.packet_number = stoi(argv[i+1]);
+                    i++;
+                }
+            }
+            cout << "n\n";
         }
         else if ((argum.compare("-t")==0) || (argum.compare("--tcp")==0)) {
+            arg.tcp = true;
             cout << "tcp\n";
         }
         else if ((argum.compare("-u")==0) || (argum.compare("--udp")==0)) {
+            arg.udp = true;
             cout << "tcp\n";
         }
         else if (argum.compare("--icmp")==0) {
+            arg.icmp = true;
             cout << "icmp\n";
         }
         else if (argum.compare("--arp")==0) {
+            arg.arp = true;
             cout << "arp\n";
         }
-        else if (argum.compare("-n")==0) {
-            cout << "n\n";
+        else {
+            cout << "unknown command\n";
         }
     }
     return 0;
 }
-void list_all_interfaces() {
 
+void list_all_interfaces() {
+    pcap_if_t *alldevs;
+    pcap_if_t *d;
+    int i=0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    
+    // Retrieve the device list from the local machine
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
+        exit(1);
+    }
+    
+    // Print the list
+    for(d= alldevs; d != NULL; d= d->next) {
+        printf("%d. %s", ++i, d->name);
+        if (d->description) {
+            printf(" (%s)\n", d->description);
+        }
+        else {
+            printf(" (No description available)\n");
+        }
+    }
+    
+    if (i == 0) {
+        printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
+        return;
+    }
+    
+    pcap_freealldevs(alldevs);
+}
+
+// Check if value if member of array of strings
+bool isMember(const std::string &value, const std::vector<std::string> &array) {
+    return std::find(array.begin(), array.end(), value) != array.end();
 }
