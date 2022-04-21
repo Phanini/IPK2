e3cbf3ba57a10eb01ff3e793a67dd68a08f54a75
Kuba Zabava <jakubphan@protonmail.com>
2022-04-21 18:54:17 +0200

    added definitions and moved includes to header

--- ipk-sniffer.hpp
+++ ipk-sniffer.hpp
@@ -3,10 +3,22 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <stdio.h>
+#define HAVE_REMOTE
+#include <pcap.h>
+#include <array>
+#include <algorithm> //std::find()
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 
 struct arguments {
-    char *interface = "all";
-    int port = -1; // if port == -1, then scan ALL ports 
+    char *interface = "";
+    char *port = ""; // if port == -1, then scan ALL ports 
     bool tcp = false;
     bool udp = false;
     bool icmp = false;
@@ -14,13 +26,75 @@ struct arguments {
     int packet_number = 1;
 };
 
-bool create_session(char *dev);
+bool create_session(arguments arg);
 arguments parse_args(arguments arg, int arg_c, char*arg_v[]);
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void print_payload(const char *payload, int len);
 
 std::vector<std::string> args_list = {"-i", "-p", "-t", "--tcp", "-u", \
                       "--udp", "--icmp", "--arp", "-n"};
 
 void list_all_interfaces();
 bool isMember(const std::string &value, const std::vector<std::string> &array);
+void print_hex_ascii_line(const u_char *payload, int len, int offset);
+
+/* default snap length (maximum bytes per packet to capture) */
+#define SNAP_LEN 1518
+
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+/* Ethernet header */
+struct sniff_ethernet {
+        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* don't fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+        u_short th_sport;               /* source port */
+        u_short th_dport;               /* destination port */
+        tcp_seq th_seq;                 /* sequence number */
+        tcp_seq th_ack;                 /* acknowledgement number */
+        u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 /* window */
+        u_short th_sum;                 /* checksum */
+        u_short th_urp;                 /* urgent pointer */
+};
 #endif
